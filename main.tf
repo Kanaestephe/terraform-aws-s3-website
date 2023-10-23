4024171da57472ecd39ae346367da762687b2071
Kanaestephe <kanaestephe@gmail.com>
2023-10-23 18:33:57 +0100

    TF Module Files Update

--- main.tf
+++ main.tf
@@ -1,67 +1,8 @@
-# Create S3 Bucket resource
-resource "aws_s3_bucket" "demo_bucket" {
-  bucket = var.bucket_name
-  tags          = var.tags
-  force_destroy = true
-}
-
-# Configure bucket acl
-resource "aws_s3_bucket_ownership_controls" "demo_bucket" {
-  bucket = aws_s3_bucket.demo_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-resource "aws_s3_bucket_public_access_block" "demo_bucket" {
-  bucket = aws_s3_bucket.demo_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "demo_bucket" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.demo_bucket,
-    aws_s3_bucket_public_access_block.demo_bucket,
-  ]
-
-  bucket = aws_s3_bucket.demo_bucket.id
-  acl    = "public-read"
-}
-
-# Configure bucket policy
-resource "aws_s3_bucket_policy" "demo_bucket_policy" {
-  bucket = aws_s3_bucket.demo_bucket.id
-  policy = data.aws_iam_policy_document.allow_everyone.json
-}
-
-data "aws_iam_policy_document" "allow_everyone" {
-  statement {
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-    actions = ["s3:GetObject",]
-
-    resources = [
-      aws_s3_bucket.demo_bucket.arn,
-      "${aws_s3_bucket.demo_bucket.arn}/*",
-    ]
-  }
-}
-
-# Define website configuration 
-resource "aws_s3_bucket_website_configuration" "demo_bucket_webssite" {
-  bucket = aws_s3_bucket.demo_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
+# Call our Custom Terraform Module which we built earlier
+module "website_s3_bucket123" {
+  source  = "app.terraform.io/hcta-demo1/s3-website/aws"
+  version = "1.0.1"
+  # insert required variables here
+  bucket_name = var.my_s3_bucket
+  tags = var.my_s3_tags
 }
--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,25 @@
-# Output variable definitions
+# Output definitions
 
-output "arn" {
-  description = "ARN of the S3 Bucket"
-  value       = aws_s3_bucket.demo_bucket.arn
+## S3 Bucket ARN
+output "website_bucket_arn" {
+  description = "ARN of the bucket"
+  value = module.website_s3_bucket.arn 
 }
 
-output "name" {
+## S3 Bucket Name
+output "website_bucket_name" {
   description = "Name (id) of the bucket"
-  value       = aws_s3_bucket.demo_bucket.id
+  value = module.website_s3_bucket.name
 }
 
-output "domain" {
-  description = "Domain Name of the bucket"
-  value       = aws_s3_bucket.demo_bucket.website_domain
+## S3 Bucket Domain
+output "website_bucket_domain" {
+  description = "Name (id) of the bucket"
+  value = module.website_s3_bucket.domain
 }
 
-output "endpoint" {
-  description = "Endpoint Information of the bucket"
-  value       = aws_s3_bucket.demo_bucket.website_endpoint
+## S3 Bucket Endpoint
+output "website_bucket_endpoint" {
+  description = "Name (id) of the bucket"
+  value = module.website_s3_bucket.endpoint
 }
